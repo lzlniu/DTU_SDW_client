03e49c8b49eedda8e2db8d72549a1830a6bed6e7
s174293 <s174293@student.dtu.dk>
2022-01-06 13:23:30 +0100

    Working client for simple DTUPay REST

--- src/main/java/dtu/pay/SimpleDTUPay.java
+++ src/main/java/dtu/pay/SimpleDTUPay.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class SimpleDTUPay {
 	Client c;
@@ -15,12 +19,18 @@ public class SimpleDTUPay {
 		r = c.target("http://localhost:8080/");
 	}
 
-	public boolean pay(int amount, String cid, String mid) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean pay(int amount, String cid, String mid) throws Exception {
+		Payment p = new Payment(cid, mid, amount);
+		Response response = r.path("payments").request().
+						   post(Entity.entity(p, MediaType.APPLICATION_JSON));
+		if (response.getStatusInfo() == Response.Status.OK) {
+			return true;
+		} else {
+			throw new Exception(response.readEntity(String.class));
+		}
 	}
 	
 	public List<Payment> getPayments(){
-		return null;
+		return r.path("payments").request().get(new GenericType<List<Payment>>(){});
 	}
 }
--- src/test/java/dtu/pay/SimpleDTUPaySteps.java
+++ src/test/java/dtu/pay/SimpleDTUPaySteps.java
@@ -2,6 +2,10 @@ package dtu.pay;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+
+import java.util.List;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,6 +15,9 @@ public class SimpleDTUPaySteps {
 	String cid, mid;
 	SimpleDTUPay dtuPay = new SimpleDTUPay();
 	boolean successful;
+	List<Payment> payments;
+	Exception e;
+	
 	@Given("a customer with id {string}")
 	public void aCustomerWithId(String cid) {
 	this.cid = cid;
@@ -21,44 +28,46 @@ public class SimpleDTUPaySteps {
 	}
 	@When("the merchant initiates a payment for {int} kr by the customer")
 	public void theMerchantInitiatesAPaymentForKrByTheCustomer(int amount) {
-	successful = dtuPay.pay(amount,cid,mid);
+		try{
+			successful = dtuPay.pay(amount,cid,mid);
+		}catch (Exception e) {this.e = e;}
 	}
 	
 	@Then("the payment is successful")
 	public void thePaymentIsSuccessful() {
-		assertTrue(successful);;
+		assertTrue(successful);
 	}
 	
-	
-	//TODO implement these tests
 	@Given("a successful payment of {int} kr from customer {string} to merchant {string}")
 	public void aSuccessfulPaymentOfKrFromCustomerToMerchant(Integer int1, String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    this.cid = string;
+	    this.mid = string2;
+	    try {
+			successful = dtuPay.pay(int1, cid, mid);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@When("the manager asks for a list of payments")
 	public void theManagerAsksForAListOfPayments() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    payments = dtuPay.getPayments();
 	}
 
 	@Then("the list contains a payments where customer {string} paid {int} kr to merchant {string}")
-	public void theListContainsAPaymentsWhereCustomerPaidKrToMerchant(String string, int int1, String string3) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void theListContainsAPaymentsWhereCustomerPaidKrToMerchant(String string, int int1, String string2) {
+	    Payment p = new Payment(string, string2, int1);
+	    assertTrue(payments.get(payments.size()-1).equals(p));
 	}
 
 	@Then("the payment is not successful")
 	public void thePaymentIsNotSuccessful() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertFalse(successful);
 	}
 
 	@Then("an error message is returned saying {string}")
 	public void anErrorMessageIsReturnedSaying(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		assertEquals(string, e.getMessage());
 	}
 }
 
